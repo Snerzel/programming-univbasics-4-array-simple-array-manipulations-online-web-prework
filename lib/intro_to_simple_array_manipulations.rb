3b5a07fabc8c8c255f7bb65065bbe458d7227dda
Cherissa Woudenberg <snerzel@comcast.net>
2020-01-12 21:08:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,7 +34,7 @@ end
 def using_concat(array)
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
-my_favorite_things.concat(more_favs)
+more_favs.concat(my_favorite_things)
 end
 
 def using_insert(array, element)
