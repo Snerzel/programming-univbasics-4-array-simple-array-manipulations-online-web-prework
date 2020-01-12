1b19bb8379af00f6e1a377efe671ae6b9c9ce8d5
Cherissa Woudenberg <snerzel@comcast.net>
2020-01-12 20:58:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ def shift_with_args(array)
 ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
 end
 
-def using_concat(array, array)
+def using_concat(array)
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
 my_favorite_things.concat(more_favs)
