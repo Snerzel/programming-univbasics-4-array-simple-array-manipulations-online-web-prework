c4b30347f30fa186f3854cd62650559ad29b201c
Cherissa Woudenberg <snerzel@comcast.net>
2020-01-12 20:41:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,7 +52,7 @@ end
 def using_delete(array, string)
 instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
 no_offense_steven = "Steven"
-instructors.delete(no_offense_steven)
+instructors.delete
 end
 
 def using_delete_at
