673cc462fd6fff31f1c39c22d284a0f8f4d7d6b4
Cherissa Woudenberg <snerzel@comcast.net>
2020-01-12 20:07:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,7 @@ end
 
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  using_pop(continents.size)
+  continents.pop(continents.size)
 
 end
 
