00a4838d2163b531ede3f9adb3a696f4a18f95d6
Cherissa Woudenberg <snerzel@comcast.net>
2020-01-12 19:54:44 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+
+def using_push(array, string)
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "violet"
+artists.push(next_artist)
+end
+
+def using_unshift(array, string)
+  artists = ["Hozier", "Ariana Grande", "Usher","Other"]
+  next_artist = "Staten Island"
+  artists.unshift(next_artist)
+end
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
+
+def pop_with_args(array)
+dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+
+end
+
+def using_shift(array)
+
+end
+
+def shift_with_args(array)
+
+end
